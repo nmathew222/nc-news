0794023922b34e484c599ce5ec5999eecaf3c72e
nmathew222 <nikhilkmathew22@gmail.com>
2022-06-13 16:21:53 +0100

    task 4 should be completed, hopefully, maybe

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 import Header from './Components/Header';
+import ShowAllArticles from './Components/ShowAllArticles';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App"> 
      <Header />
+     <Routes>
+       <Route path='/' element={<ShowAllArticles />}></Route>
+
+     </Routes>
     
     
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/ShowAllArticles.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+function ShowAllArticles({ selectedArticles }) {
+  const [allArticles, setAllArticles] = useState([]);
+  const [IsLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(`https://nc-news-nikhil.herokuapp.com/api/articles`)
+      .then((res) => {
+        return res.json();
+      })
+      .then(({ articles }) => {
+        console.log(articles);
+        setAllArticles(articles);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (IsLoading) {
+    return <p>Loading......</p>;
+  }
+  return (
+    <>
+      <h4>Show All articles: </h4>
+
+      <div className="ShowAllArticles">
+        {allArticles.map((article) => {
+          return (
+            <div key={article.article_id} className="ShowAllArticles__Article">
+              <h4 className="thumbnail">
+                {article.title} 
+              </h4>
+
+              
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default ShowAllArticles;
