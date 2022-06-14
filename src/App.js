205e6073070af2b463dff9eddf8fbbadec26194c
nmathew222 <nikhilkmathew22@gmail.com>
2022-06-14 10:56:22 +0100

    more styling

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Header from './Components/Header';
 import ShowAllArticles from './Components/ShowAllArticles';
 import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App"> 
      <Header />
+     <Navbar />
      <Routes>
        <Route path='/' element={<ShowAllArticles />}></Route>
 
---
+++ src/Components/Navbar.js
@@ -0,0 +1,14 @@
+function Navbar () {
+    return (
+
+      <div className="Navbar">
+
+      <div className="Navbar__item"><a href="/">Home</a></div>
+      
+
+      </div>
+
+    )
+}
+
+export default Navbar;
