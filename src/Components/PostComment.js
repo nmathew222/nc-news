9195f8bd24914216f23bd9319080be0a0e2a1d8d
nmathew222 <nikhilkmathew22@gmail.com>
2022-06-16 15:34:40 +0100

    comment box made and validation added

--- src/Components/PostComment.js
+++ src/Components/PostComment.js
@@ -1,18 +1,23 @@
 import { useState, useEffect } from "react";
 import { postComment } from "../utils/api";
 
-export default function PostComment({article_id}) {
+export default function PostComment({ article_id }) {
   const [body, setBody] = useState("");
   const [username, setUsername] = useState("");
+  const [goodRequest, setGoodRequest] = useState(false);
+  const [badRequest, setBadRequest] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     postComment(article_id, username, body)
-.then((createdComment) => {
+      .then((createdComment) => {
+        setGoodRequest(true);
         setBody("");
         setUsername("");
       })
       .catch((err) => {
+        setGoodRequest(false);
+        setBadRequest(true);
         setBody("");
         setUsername("");
       });
@@ -21,9 +26,19 @@ export default function PostComment({article_id}) {
   return (
     <div className="postCommentForm">
       <form className="postComment" method="get" onSubmit={handleSubmit}>
+        <div>
+          {goodRequest === true ? (
+            <p>Your comment is posted</p>
+          ) : badRequest === true ? (
+            <p>Invalid username or missing comment </p>
+          ) : null}{" "}
+        </div>
+
         <label>
           <p>Post comment:</p>
-          <input
+          <textarea
+            rows="4"
+            cols="50"
             className="postCommentTxtBx"
             type="text"
             placeholder="comment..."
@@ -31,7 +46,9 @@ export default function PostComment({article_id}) {
             onChange={(event) => {
               setBody(event.target.value);
             }}
-          ></input>
+          ></textarea>
+          <br></br>
+          <br></br>
           <br></br>
           <input
             className="postCommentUsernameBx"
